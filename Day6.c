007105b69d78ef7e0bb38762b6ed49a50227136a
karman2002ba-netizen <karman2002ba@gmail.com>
2026-02-06 08:30:53 +0530

    Add files via upload

---
+++ Day6.c
@@ -0,0 +1,30 @@
+//Remove Duplicates from Sorted Array
+#include <stdio.h>
+
+int removeDuplicates(int* nums, int numsSize) {
+    if (numsSize == 0) return 0;
+
+    int i = 0;
+    for (int j = 1; j < numsSize; j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}
+
+int main() {
+    int nums[] = {1, 1, 2, 2, 3};
+    int size = sizeof(nums) / sizeof(nums[0]);
+
+    int k = removeDuplicates(nums, size);
+
+    printf("Unique count: %d\n", k);
+    printf("Array after removing duplicates: ");
+    for (int i = 0; i < k; i++) {
+        printf("%d ", nums[i]);
+    }
+
+    return 0;
+}
