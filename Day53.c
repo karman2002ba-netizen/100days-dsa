c3fdfe28105ff390d5eac0aa847e0a831dd6f0c1
karman2002ba-netizen <karman2002ba@gmail.com>
2026-03-25 11:13:23 +0530

    Add files via upload

---
+++ Day53.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node* root = createNode(arr[0]);
+
+    struct Node* queue[n];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+    int i = 1;
+
+    while (i < n) {
+        struct Node* curr = queue[front++];
+
+        
+        if (arr[i] != -1) {
+            curr->left = createNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        
+        if (i < n && arr[i] != -1) {
+            curr->right = createNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+struct QNode {
+    struct Node* node;
+    int hd;
+};
+
+
+void verticalOrder(struct Node* root) {
+    if (!root) return;
+
+    
+    struct QNode queue[1000];
+    int front = 0, rear = 0;
+
+    
+    int map[2000][100]; 
+    int count[2000] = {0};
+
+    int offset = 1000; 
+
+    queue[rear++] = (struct QNode){root, 0};
+
+    int minHD = 0, maxHD = 0;
+
+    while (front < rear) {
+        struct QNode temp = queue[front++];
+        struct Node* curr = temp.node;
+        int hd = temp.hd;
+
+       
+        map[hd + offset][count[hd + offset]++] = curr->data;
+
+        if (hd < minHD) minHD = hd;
+        if (hd > maxHD) maxHD = hd;
+
+        if (curr->left)
+            queue[rear++] = (struct QNode){curr->left, hd - 1};
+        if (curr->right)
+            queue[rear++] = (struct QNode){curr->right, hd + 1};
+    }
+
+   
+    for (int i = minHD; i <= maxHD; i++) {
+        for (int j = 0; j < count[i + offset]; j++) {
+            printf("%d ", map[i + offset][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* root = buildTree(arr, n);
+
+    verticalOrder(root);
+
+    return 0;
+}
