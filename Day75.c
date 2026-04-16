ca9cb08d60e25ecaf5ae598b734fa05422eb7199
karman2002ba-netizen <karman2002ba@gmail.com>
2026-04-16 20:21:54 +0530

    Add files via upload

---
+++ Day75.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int longestZeroSumSubarray(int arr[], int n) {
+    int maxLen = 0;
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            sum += arr[j];
+            if (sum == 0) {
+                int len = j - i + 1;
+                if (len > maxLen)
+                    maxLen = len;
+            }
+        }
+    }
+
+    return maxLen;
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Longest zero-sum subarray length: %d\n", longestZeroSumSubarray(arr, n));
+
+    return 0;
+}
